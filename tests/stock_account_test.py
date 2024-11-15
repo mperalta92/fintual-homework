86d1c1a97b887bbdcdb5c85cba4dc0dc73afabaa
Matías Peralta <matias.peralta@velaindustriesgroup.com>
2024-11-14 23:34:12 -0300

    add test to StockAccount class

---
+++ tests/stock_account_test.py
@@ -0,0 +1,13 @@
+from unittest import TestCase, mock
+from app.stock_account import StockAccount
+
+class StockAccountTest(TestCase):
+
+    def test_price(self):
+        name = 'AAPL'
+        quantity = 10
+        date = '2023-01-09'
+        stock = mock.Mock()
+        stock.price.return_value = 87.36
+        stock_account = StockAccount(stock, quantity)
+        self.assertEqual(stock_account.price(date), 873.6)
